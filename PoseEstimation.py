229a8bda26cd6d92d18b30e13e0ed88edba54923
Vanshika Bhatia <vanshika18042000@gmail.com>
2021-05-02 13:46:53 +0530

    Creation of Pose Module sucessful

--- PoseEstimation.py
+++ PoseEstimation.py
@@ -34,6 +34,7 @@ while True:
     fps = 1/(cTime-pTime)
     pTime = cTime
 
+    cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
     cv2.imshow("Image", img)
 
     cv2.waitKey(5)
